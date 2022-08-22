b5b94bd287589fea7a56365219a03eb55482e124
Lionel Owono <lowono@hcub3.fr>
2022-08-22 11:18:06 +0200

    feat: list user web service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,24 +6,33 @@ use App\Service\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/user', name: 'esplt_user_')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user')]
-    public function index(): JsonResponse
+
+    private Serializer $serializer;
+
+    public function __construct(SerializerInterface $serializer)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $this->serializer = $serializer;
     }
 
+    /**
+     * @throws ExceptionInterface
+     */
     #[Route('/list', name: 'list')]
-    public function list(UserManager $manager)
+    public function list(UserManager $manager): JsonResponse
     {
         $this->denyAccessUnlessGranted('list');
 
         $users = $manager->list();
+
+        $data = $this->serializer->normalize($users, null, ['groups' => 'list']);
+
+        return new JsonResponse($data, 200);
     }
 }
